3d3babb642ef273a7bb2a5078da3c93d45ba9dc1
rodrigo12camargo <rodrigo_1320114154@uptecamac.edu.mx>
2024-04-24 22:38:21 -0600

    template, falta el css (solo index)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('', function () {
+    return view('template/index');
 });
+
+/*Route::get('about', function () {
+    return view('template/about');
+});
+
+Route::get('contact', function () {
+    return view('template/contact');
+});
+
+Route::get('service', function () {
+    return view('template/service');
+});
+
+Route::get('portfolio', function () {
+    return view('template/project');
+});
+
+Route::get('team', function () {
+    return view('template/team');
+});
+
+Route::get('testimonial', function () {
+    return view('template/testimonial');
+});
+
+Route::get('feature', function () {
+    return view('template/featrure');
+});
+
+Route::get('404', function () {
+    return view('template/404');
+});*/
+
+
